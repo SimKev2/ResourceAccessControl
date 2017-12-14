767c0cef906c0f32739cf9ddc294d87d28b8208c
simkev2 <simkev2@gmail.com>
2017-12-13 20:38:31 -0600

    Scheduling:  - Added priority inheritance scheduling

--- app/src/main/java/com/example/cpre458/resourceaccesscontrol/Scheduling.java
+++ app/src/main/java/com/example/cpre458/resourceaccesscontrol/Scheduling.java
@@ -178,6 +178,88 @@ public class Scheduling {
         return schedule;
     }
 
+    public ArrayList<String> PriorityInheritance() throws Exception {
+        ArrayList<String> schedule = new ArrayList<>();
+        ArrayList<Task> taskQueue = new ArrayList<>();
+        Task currentTask = null;
+        Task prevTask = null;
+        Task checkTask;
+        int currentTime = 0;
+
+        for (Resource r : this.resources) {
+            r.unlock(); // Make sure all needed resources are unlocked
+        }
+
+        while (currentTime < this.scheduleDuration) {
+            System.out.format("Time: %d\n", currentTime);
+
+            for (Task t : this.taskList) {
+                if (currentTime % t.period == 0) {
+                    Task newTask = t.clone();
+                    newTask.taskNum = currentTime / t.period;
+                    taskQueue.add(newTask);
+                    System.out.format("\tTask Arrived: %s\n", newTask);
+                }
+            }
+
+            if (taskQueue.isEmpty()) {
+                System.out.format("\tNo task\n\n");
+                currentTime += 1;
+                currentTask = null;
+                prevTask = currentTask;
+                schedule.add(null);
+                continue;
+            }
+
+            Collections.sort(taskQueue);
+            for (int i = 0; i < taskQueue.size(); i++) {
+                checkTask = taskQueue.get(i);
+                if (currentTask == checkTask) {
+                    break;
+                }
+
+                if (checkTask.canExecute()) {
+                    checkTask.startExecution();
+                    currentTask = checkTask;
+                    break;
+                }
+                else {
+                    for (Resource r : checkTask.requiredResources) {
+                        if (r.owner != null) {
+                            r.owner.priority = checkTask.priority;
+                            currentTask = r.owner;
+                            System.out.format("\tRaising %s to priority %d\n", r.owner, checkTask.priority);
+                        }
+                    }
+                    break;
+                }
+            }
+
+            if (prevTask != null && currentTask != prevTask) {
+                System.out.format("\tPreempted %s, with %s\n", prevTask, currentTask);
+            }
+
+            if (currentTask == null) {
+                throw new Exception("Schedule not feasible");
+            }
+
+            System.out.format("\tExecuting task: %s\n", currentTask);
+            schedule.add(currentTask.toString());
+            currentTime += 1;
+            currentTask.remainingExecutionTime -= 1;
+            if (currentTask.remainingExecutionTime == 0) {
+                taskQueue.remove(currentTask);
+                currentTask.releaseResources();
+                System.out.format("\tFinished task: %s\n", currentTask);
+                currentTask = null;
+                prevTask = null;
+            } else {
+                prevTask = currentTask;
+            }
+        }
+        return schedule;
+    }
+
     private static int gcd(int a, int b) {
         int temp;
         while (b > 0) {
--- app/src/test/java/com/example/cpre458/resourceaccesscontrol/SchedulingTest.java
+++ app/src/test/java/com/example/cpre458/resourceaccesscontrol/SchedulingTest.java
@@ -70,4 +70,36 @@ public class SchedulingTest {
         Scheduling scheduler = new Scheduling(testList, reqs);
         System.out.format(scheduler.PriorityCeiling().toString());
     }
+
+    @Test
+    public void scheduleInheritanceTest() throws Exception {
+        Resource[] reqs = new Resource[2];
+        Resource r1 = new Resource();
+        Resource r2 = new Resource();
+
+        reqs[0] = r1;
+        reqs[1] = r2;
+
+        Resource[] senderReqs = new Resource[1];
+        Resource[] cpuReqs = new Resource[2];
+        Resource[] ioReqs = new Resource[1];
+
+        senderReqs[0] = r1;
+
+        cpuReqs[0] = r1;
+        cpuReqs[1] = r2;
+
+        ioReqs[0] = r2;
+
+        ArrayList<Task> testList = new ArrayList<>();
+        testList.add(new Task(
+                "Sender", 2, 10, 10, 0, senderReqs));
+        testList.add(new Task(
+                "CPU", 2, 5, 5, 0, cpuReqs));
+        testList.add(new Task(
+                "IO", 4, 15, 15, 0, ioReqs));
+
+        Scheduling scheduler = new Scheduling(testList, reqs);
+        System.out.format(scheduler.PriorityCeiling().toString());
+    }
 }
